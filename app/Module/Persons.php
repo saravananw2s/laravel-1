5e530acac430d8b9c0581fd2ca318e1bc0fad07a
Sha <saravanan@way2smile.com>
2018-04-21 14:16:22 +0530

    test

--- app/Module/Persons.php
+++ app/Module/Persons.php
@@ -3,8 +3,12 @@
 namespace App\Module;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class Persons extends Model
 {
+	    use SoftDeletes;
+
     protected $table = 'Persons';
     public $timestamps = false;
     protected $guarded = [];
